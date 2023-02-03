37762c129f193d2dce598928deaa657ee621c9f3
cozz-py <bencozzens13@gmail.com>
2023-02-03 08:02:16 -0500

    Generates 20 random strings

--- RandomClass/Program.cs
+++ RandomClass/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Type the number of doubles to generate: ");
+        string line = Console.ReadLine();
+
+        /*while (line != null)
+        {
+            if (int.TryParse(line))
+        }*/
+        
+        Random random = new Random();
+        double[] randomDoubles = new double[20]; //sub in 20 for parsed int read from line
+        for (int i = 0;i < randomDoubles.Length; i++)
+        {
+            double value = random.NextDouble();
+            randomDoubles[i] = value;
+            Console.WriteLine(value.ToString("0.00"));
+        }
+        Console.WriteLine("There are " + randomDoubles.Length + " doubles randomly generated.");
+    }
+}
