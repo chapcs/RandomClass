69c01b4bc402cec4f2570e7ac5399e0243cc8148
cozz-py <bencozzens13@gmail.com>
2023-02-04 10:52:41 -0500

    while loop with parse

--- RandomClass/Program.cs
+++ RandomClass/Program.cs
@@ -5,20 +5,43 @@
     {
         Console.Write("Type the number of doubles to generate: ");
         string line = Console.ReadLine();
+        string m = "exit";
 
-        /*while (line != null)
+        //while loop to run program until exited
+        while (line.ToString() != "0")
         {
-            if (int.TryParse(line))
-        }*/
-        
-        Random random = new Random();
-        double[] randomDoubles = new double[20]; //sub in 20 for parsed int read from line
-        for (int i = 0;i < randomDoubles.Length; i++)
-        {
-            double value = random.NextDouble();
-            randomDoubles[i] = value;
-            Console.WriteLine(value.ToString("0.00"));
+            //Parse and loop utilized from PickRandomCards
+            if (int.TryParse(line, out int n))
+            {
+                Random random = new Random();
+                double[] randomDoubles = new double[n];
+                for (int i = 0; i < randomDoubles.Length; i++)
+                {
+                    //generation of doubles
+                    double value = random.NextDouble();
+                    randomDoubles[i] = value;
+                    Console.WriteLine(value.ToString("0.00"));
+                }
+                Console.WriteLine("There are " + randomDoubles.Length + " doubles randomly generated.");
+                Console.Write("\nEnter another amount to generate or 'exit': ");
+                string last = Console.ReadLine();
+
+                //sets result true for any case of 'exit'
+                bool result = string.Equals(last, m, StringComparison.CurrentCultureIgnoreCase);
+                if (result == true)
+                {
+                    Console.Write("\nThank you for experimenting with the Random class! \nGoodbye");
+                    Thread.Sleep(3000);
+                    Environment.Exit(0);
+                }
+                line = last;
+                continue;
+            }
+            else
+            {
+                Console.Write("You entered a string, please enter an integer:");
+                line = Console.ReadLine();
+            }
         }
-        Console.WriteLine("There are " + randomDoubles.Length + " doubles randomly generated.");
     }
 }
